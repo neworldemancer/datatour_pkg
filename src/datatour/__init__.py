a0606663a6d2e3b7d648df359a5eee7d25ed8bda
neworldemancer <neworldemancer@gmail.com>
2024-06-20 12:24:10 +0200

    'formating'

--- src/datatour/__init__.py
+++ src/datatour/__init__.py
@@ -1,8 +1,15 @@
-"""Seeing is important. `datatour` - allows you to see your data in it's native dimension.
-Currently implemented as a `plotly` scatter plot projected from it's original dimension in the 2D on the screen with timeline animation inspired by GrandTour and common sense.
+"""
+Visualization in its native dimension.
+
+Seeing is important. `datatour` - allows you to see your data in its
+native dimension. Currently implemented as a `plotly` scatter plot projected
+from its original dimension in the 2D on the screen with timeline animation
+inspired by GrandTour and common sense.
 """
 
 from importlib.metadata import PackageNotFoundError, version
+
+# from . import datatour
 
 try:
     __version__ = version("datatour")
